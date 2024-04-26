ce04d328865b8c6370781736a27e5877a164a0f1
krispycrem <obananas073@gmail.com>
2024-04-27 00:43:48 +0300

    Add validation to backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Person = require('./models/person')
 app.use(express.static('dist'))
 
 const cors = require('cors')
-const morgan = require("morgan")
+const morgan = require('morgan')
 
 
 app.use(express.json())
@@ -20,7 +20,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
-
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
@@ -47,7 +49,7 @@ app.get('/api/info', (request, response) => {
   })
 })
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
     if (body.name === undefined || body.number === undefined) {
       return response.status(400).json({ error: 'name or number is missing' })
@@ -61,6 +63,8 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
+
   })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -84,14 +88,11 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, 
+    { name, number }, 
+    { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
